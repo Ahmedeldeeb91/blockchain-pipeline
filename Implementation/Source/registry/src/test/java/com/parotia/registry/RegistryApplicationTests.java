4908df0705c94d1a33fabce306b201d791b03b78
Ahmed El-Deeb <ahmed.hassanin.eldeeb@gmail.com>
2025-09-05 19:20:53 +0300

    service registry

---
+++ Implementation/Source/registry/src/test/java/com/parotia/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parotia.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
